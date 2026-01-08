6077fb08340b97cd93596a9ec37950259c389b62
Yujin1219 <jin-1219@naver.com>
2026-01-08 11:37:01 +0900

    Init: 프로젝트 초기세팅

---
+++ src/test/java/com/example/speakOn/SpeakOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.speakOn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

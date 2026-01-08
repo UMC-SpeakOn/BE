fa1c09ac191439e24fd703b411f0730bd7916036
Yujin1219 <jin-1219@naver.com>
2026-01-08 14:18:12 +0900

    Init: 공통 응답 코드 작성

---
+++ src/main/java/com/example/speakOn/global/apiPayload/code/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.example.speakOn.global.apiPayload.code;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter(AccessLevel.PROTECTED)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Setter(AccessLevel.PROTECTED)
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}

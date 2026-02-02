15c53c804fb4e8053e136913b3b557713a5cf7d6
Yujin1219 <jin-1219@naver.com>
2026-02-02 22:28:10 +0900

    Feat: 구글 소셜 로그인 구현

--- src/main/java/com/example/speakOn/domain/auth/controller/AuthController.java
+++ src/main/java/com/example/speakOn/domain/auth/controller/AuthController.java
@@ -35,16 +35,17 @@ public class AuthController {
         return ApiResponse.onSuccess(response);
     }
 
-//    @Operation(
-//            summary = "구글 인가 코드로 로그인",
-//            description = "구글 로그인 후 리다이렉트된 URL의 code 파라미터만 전달하면 백엔드가 토큰 발급부터 로그인까지 모두 처리합니다. " +
-//                    "(개발시) Google OAuth 2.0 콘솔에서 발급받은 클라이언트 ID를 사용하여 인가 코드를 받아 요청 바디에 전달해주세요."
-//    )
-//    @PostMapping("/google")
-//    public ApiResponse<AuthResponse.SocialLoginResponseDTO> loginWithGoogleCode(
-//            @Valid @RequestBody GoogleDTO.CodeRequest request
-//    ) {
-//        AuthResponse.SocialLoginResponseDTO response = authService.loginWithGoogleCode(request.code(), request.redirectUri());
-//        return ApiResponse.onSuccess(response);
-//    }
+    @Operation(
+            summary = "구글 인가 코드로 로그인",
+            description = "구글 로그인 후 리다이렉트된 URL의 code 파라미터만 전달하면 백엔드가 토큰 발급부터 로그인까지 모두 처리합니다. " +
+                    "(개발시) 아래 url에서 인가 코드를 받아 요청 바디에 전달해주세요." +
+                    "https://accounts.google.com/o/oauth2/v2/auth?client_id=170876738810-o5t93iktkb7qi76t5t3bba5n7sahb4o8.apps.googleusercontent.com&redirect_uri=http://localhost:5173/login/oauth2/code/google&response_type=code&scope=email+profile"
+    )
+    @PostMapping("/google")
+    public ApiResponse<AuthResponse.SocialLoginResponseDTO> loginWithGoogleCode(
+            @Valid @RequestBody GoogleDTO.CodeRequest request
+    ) {
+        AuthResponse.SocialLoginResponseDTO response = authService.loginWithGoogleCode(request.code(), request.redirectUri());
+        return ApiResponse.onSuccess(response);
+    }
 }
